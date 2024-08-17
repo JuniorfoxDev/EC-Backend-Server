4ff08675c8f06b9a9ce9fd926d2da2fd941af120
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2024-08-17 19:57:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,21 +42,7 @@ mongoose.connect(mongoURI, {
 
 // User model
 const User = require('./models/Register')
-
-const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    description: { type: String, required: true },
-    sizes:{type: [String], required: true},
-    images: [{
-        filename: String,
-        id: mongoose.Schema.Types.ObjectId,
-        url: String
-    }]
-});
-
-const Product = mongoose.model('Product', productSchema);
-
+const Product = require('./models/Product')
 // Multer setup
 const storage = multer.memoryStorage();
 const upload = multer({ storage, limits: { fileSize: 50 * 1024 * 1024 } });
